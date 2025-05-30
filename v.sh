cdf3a83944d5d7331888e57bc7471fd596b10332
Jaxxen <jc1kp@proton.me>
2025-05-30 12:49:05 -0500

    Update v.sh

--- v.sh
+++ v.sh
@@ -15,7 +15,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
 # Install packages
-packages="libayatana-appindicator-devel btop blender cairo-devel cmake fastfetch font-awesome fontconfig galculator gimp gettext git github-cli gstreamer1-devel gtk+3 gtk-engine-murrine gtk-layer-shell hyprcursor hypridle hyprlang hyprlock hyprpaper hyprutils hyprwayland-scanner hyprland-devel hyprland-protocols libdrm-devel libgbm-devel libglvnd-devel libinput-devel libjxl-devel libjpeg-turbo-devel libnotify-devel libseat-devel libwebp-devel libxkbcommon-devel libreoffice librewolf mako mpv ninja neovim pango-devel pavucontrol perl pipewire prismlauncher qbittorrent re2-devel slurp steam starship tomlplusplus unzip wayland-devel wayland-protocols wev wl-clipboard wofi wine-gecko wine-mono xdg-desktop-portal-hyprland yazi zsh"
+packages="blender btop cairo-devel cmake eog fastfetch font-awesome fontconfig foot galculator gcc gettext gimp gnome-keyring gnome-themes-extra grim gstreamer1-devel gtk+3 gtk-engine-murrine gtk-layer-shell gzip hyprcursor hypridle hyprland hyprland-devel hyprland-protocols hyprlang hyprlock hyprpaper hyprutils hyprwayland-scanner jq libayatana-appindicator-devel libdrm-devel libgbm-devel libglvnd-devel libinput-devel libjpeg-turbo-devel libjxl-devel libnotify-devel libreoffice librewolf libseat-devel libwebp-devel libxkbcommon-devel mako mpv neovim ninja nwg-look pango-devel pavucontrol perl pipewire PrismLauncher python-clickgen python3 qbittorrent re2-devel sdbus-cpp Signal-Desktop slurp starship steam tomlplusplus tuigreet ufw unzip Waybar wayland-devel wayland-protocols wev wine-gecko wine-mono wl-clipboard wofi xdg-desktop-portal-hyprland yazi zsh"
 sudo xbps-install -y $packages
 
 # Hyprsunset
