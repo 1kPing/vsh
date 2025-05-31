176f495b1cc163435d8857c00dd298278e3855a1
Jaxxen <jc1kp@proton.me>
2025-05-31 17:14:35 -0500

    Update v.sh

--- v.sh
+++ v.sh
@@ -16,7 +16,7 @@ echo "repository=https://github.com/index-0/librewolf-void/releases/latest/downl
 sudo xbps-install -Syu
 
 # Install packages
-packages="NetworkManager PrismLauncher Signal-Desktop Waybar alsa-pipewire blender btop elogind fastfetch font-awesome foot galculator gimp git gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprland-protocols imv libreoffice librewolf mako mpv neovim nwg-look pavucontrol pipewire pipewire-devel polkit qbittorrent sassc seatd starship steam tuigreet ufw wev wine wine-gecko wine-mono wofi xdg-desktop-portal-hyprland hyprlock hyprpaper yazi zsh"
+packages="NetworkManager PrismLauncher Signal-Desktop Waybar alsa-pipewire blender btop elogind fastfetch font-awesome foot galculator gimp git gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprland-protocols hyprlock hyprpaper imv libreoffice librewolf mako mpv neovim nwg-look pavucontrol pipewire pipewire-devel polkit qbittorrent sassc seatd starship steam ufw wev wine wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi zsh"
 sudo xbps-install -Syu $packages
 
 sudo mv ~/eww /bin
