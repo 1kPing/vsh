f96645906b77cd5c4169fe4ab9d236b4b9851798
Jaxxen <jc1kp@proton.me>
2025-06-02 14:09:30 -0500

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,34 +1,52 @@
 #!/bin/dash
 
-echo "~ is being backed up to /home/old~"
-sudo mkdir /home/old~
-sudo mv ~/* /home/old~
-sudo mv /home/old~/vsh ~
-cd ~/vsh
-find . -maxdepth 1 -mindepth 1 -exec mv -f {} ~ \;
+RED='\033[0;31m'
+echo "Hit enter to proceed..."
+read answer
+if [ "$answer" = "noconf" ]; then
+    echo "Backing up ~ to /home/old~"
+    sudo mkdir /home/old~
+    sudo mv ~/* /home/old~
+    sudo mv /home/old~/vsh ~
+    cd ~/vsh
+    find . -maxdepth 1 -mindepth 1 -exec mv -f {} ~ \;
+fi
 cd
 
 # Update system and install repo packages
 sudo xbps-install -yu xbps 
 sudo xbps-install -Syu void-repo-multilib void-repo-nonfree void-repo-multilib-nonfree
-echo "repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc" | sudo tee /etc/xbps.d/hyprland-mirror.conf
 echo "repository=https://github.com/index-0/librewolf-void/releases/latest/download/" | sudo tee /etc/xbps.d/librewolf-mirror.conf
 sudo xbps-install -Syu
 
 # Install packages
-packages="NetworkManager PrismLauncher Signal-Desktop Waybar alsa-pipewire blender btop elogind fastfetch font-awesome foot galculator gimp git gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprland-protocols hyprlock hyprpaper imv libreoffice librewolf mako mpv neovim nwg-look pavucontrol pipewire pipewire-devel polkit qbittorrent sassc seatd starship steam sway ufw wev wine wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi zsh"
-sudo xbps-install -Syu $packages
+packages="NetworkManager PrismLauncher Signal-Desktop alsa-pipewire blender btop elogind fastfetch font-awesome foot galculator gimp git gnome-keyring gnome-themes-extra gtk-engine-murrine imv libreoffice librewolf mako mpv neovim nwg-look pavucontrol pipewire pipewire-devel polkit qbittorrent sassc seatd starship steam ufw wev wine wine-gecko wine-mono wofi yazi zsh"
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/kontr0x/github-desktop-install/main/installGitHubDesktop.sh"
-sudo mv ~/eww /bin
-sudo mv ~/alarm-clock-applet /bin
-sudo mv ~/theclicker /bin
+for package in $packages; do
+  sudo xbps-install -y "$package"
+done
+
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/polkitd /var/service
+sudo ln -s /etc/sv/seatd /var/service
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/kontr0x/github-desktop-install/main/installGitHubDesktop.sh)"
+sudo mv ~/binaries/* /bin
+mv ~/GithubDesktop* /bin/github
+
+## echo "Do you want to install hypr stuff? (y/n)"
+## read answer
+## if [ "$answer" = "y" ]; then
+##     echo "repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc" | sudo tee /etc/xbps.d/hyprland-mirror.conf
+##     sudo xbps-install -Syu hyprland hyprland-protocols hyprlock hyprpaper xdg-desktop-portal-hyprland
+## fi
 
 echo "Do you want to install discord with flatpak? (y/n)"
 read answer
 if [ "$answer" = "y" ]; then
     sudo xbps-install -y flatpak
-    flatpak install com.discordapp.Discord -y
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    sudo flatpak install com.discordapp.Discord -y
 fi
 
 echo "Do you want to install nvidia stuff? (y/n)"
@@ -37,7 +55,7 @@ if [ "$answer" = "y" ]; then
     sudo xbps-install -y libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit nvidia nvidia-libs-32bit linux-firmware-nvidia
 fi
 
-# Configuration
+# More configuration
 ~/graphite-gtk-theme/install.sh --tweaks rimless black
 gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-Dark'
 gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
@@ -54,10 +72,5 @@ rm -r ~/graphite-gtk-theme
 rm ~/LICENSE
 rm ~/README.md
 rm ~/v.sh
-
-# Startup services
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/polkitd /var/service
-sudo ln -s /etc/sv/seatd /var/service
 
 echo "finished, reboot your computer"
