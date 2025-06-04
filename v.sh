c69c64d3aca32e024501bff7e5959c531ed03eed
Jaxxen <jc1kp@proton.me>
2025-06-04 17:47:07 -0500

    Update v.sh

--- v.sh
+++ v.sh
@@ -2,11 +2,6 @@
 
 trap 'exit 1' INT
 
-if [ ! -d "~/vsh" ]; then
-    echo "Error: vsh directory not found in ~ ; Move it there and try again."
-    exit 1
-fi
-
 echo "!! Everything currently in ~ will be moved to /home/old~N !!"
 sleep 1
 echo "Hit enter to proceed..."
@@ -33,7 +28,7 @@ else
     cd
     sudo rmdir "$target_dir"/vsh
     sudo mkdir -p /usr/share/fonts/TTF
-    sudo rsync ~/TTF/* /usr/share/fonts/TTF
+    sudo mv -f ~/TTF/* /usr/share/fonts/TTF
     rm -rf ~/TTF
     rm -rf ~/.git
     rm ~/LICENSE
@@ -45,17 +40,13 @@ sudo xbps-install -Syu void-repo-multilib void-repo-nonfree void-repo-multilib-n
 echo "repository=https://github.com/index-0/librewolf-void/releases/latest/download/" | sudo tee /etc/xbps.d/librewolf-mirror.conf
 sudo xbps-install -Syu
 
-packages="libxcb-devel pam-devel zig NetworkManager PrismLauncher Signal-Desktop alacritty alsa-pipewire awesome blender brightnessctl btop dunst fastfetch font-awesome galculator gimp git gnome-keyring gnome-themes-extra gtk-engine-murrine i3lock imv libreoffice librewolf libxcb mpv neovim nwg-look pam pavucontrol pipewire pipewire-devel qbittorrent rofi sassc scrot starship steam ufw wine wine-gecko wine-mono xev xorg xauth yazi zsh"
+packages="NetworkManager PrismLauncher Signal-Desktop alacritty alsa-pipewire awesome blender btop dunst fastfetch font-awesome galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine i3lock imv libreoffice librewolf mpv neovim nwg-look pavucontrol pipewire qbittorrent rofi sassc scrot starship steam ufw wine wine-gecko wine-mono yazi zsh"
 
 for package in $packages; do
     sudo xbps-install -y "$package"
 done
 
 sudo ln -s /etc/sv/dbus /var/service
-cd ly
-sudo zig build installexe -Dinit_system=runit
-sudo ln -s /etc/sv/ly /var/service/
-sudo rm /var/service/agetty-tty2
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/kontr0x/github-desktop-install/main/installGitHubDesktop.sh)"
 sudo mv ~/binaries/* /bin
